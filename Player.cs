f1813d55b42f0317118ad23da5ef89e9aca901e9
DonggeonKim1012 <60847197+DonggeonKim1012@users.noreply.github.com>
2021-05-10 18:44:44 +0900

    Update Player.cs
    corrected a few typos

--- Player.cs
+++ Player.cs
@@ -12,20 +12,18 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = GetComponent<PlayerController>();
-        viewCamera = viewCamera.main;
+        viewCamera = Camera.main;
     }
 
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
-        Ray ray = viewCamera.ScreenPointToRay(moveInput.mouseposition);
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        
-        
-        float rayDistance;
+
         if (groundPlane.Raycast(ray, out float rayDistance))
         {
             Vector3 point = ray.GetPoint(rayDistance);
