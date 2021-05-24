27a0e01ebc8e817c5c9ecc4dc3bb4b4a88203ce2
DonggeonKim1012 <60847197+DonggeonKim1012@users.noreply.github.com>
2021-05-24 23:59:42 +0900

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
-public class Enemy : MonoBehaviour
+public class Enemy : LivingEntity
 {
     UnityEngine.AI.NavMeshAgent pathfinder;
     Transform target;
 
-    // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
+        base.Start();
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(UpdatePath());
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -28,7 +27,7 @@ public class Enemy : MonoBehaviour
 
         while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 1, target.position.z);
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
             pathfinder.SetDestination(target.position);
             yield return new WaitForSeconds(refreshRate);
         }
