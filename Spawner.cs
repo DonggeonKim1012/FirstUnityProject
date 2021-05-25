ac7b125fdb477c983885fc91245c3cba576ef481
DonggeonKim1012 <60847197+DonggeonKim1012@users.noreply.github.com>
2021-05-25 23:39:27 +0900

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Enemy enemy;
+
+    Wave currentWave;
+    int currentWaveNumber;
+
+    int enemiesReamainingToSpawn;
+    int enemiesReamainingAlive;
+    float nextSpawnTime;
+
+    void Start()
+    {
+        NextWave();
+    }
+
+    void Update()
+    {
+        if(enemiesReamainingToSpawn > 0 && Time.time > nextSpawnTime)
+        {
+            enemiesReamainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+            spawnedEnemy.OnDeath += OnEnemyDeath;
+        }
+    }
+
+    void OnEnemyDeath()
+    {
+        enemiesReamainingAlive--;
+
+        if(enemiesReamainingAlive ==0)
+        {
+            NextWave();
+        }
+    }
+
+    void NextWave()
+    {
+        currentWaveNumber++;
+        print("Wave " + currentWaveNumber);
+        if (currentWaveNumber - 1 < waves.Length)
+        {
+            currentWave = waves[currentWaveNumber - 1];
+
+            enemiesReamainingToSpawn = currentWave.enemyCount;
+            enemiesReamainingAlive = enemiesReamainingToSpawn;
+        }
+    }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public int enemyCount;
+        public float timeBetweenSpawns;
+    }
+}
