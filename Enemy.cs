30502b596453e11cb24fcc7de2ea9208237592c6
DonggeonKim1012 <60847197+DonggeonKim1012@users.noreply.github.com>
2021-05-26 23:58:12 +0900

    Update Enemy.cs
    Attack() enumerator: changes the position of the enemy closer to the player and then back to the original location as if its biting.
    
    +3 states added to mark if the enemy is chasing the player, attacking, or staying still.
    This was done to prevent the enemy from re-calculating the target's position while attacking.
    
    +variables myCollisionRadius and targetCollisionRadius was used to change the target location of the enemy,
    as it was going inside the player object previously 
    
    +skinMaterial and originalColour variables were made to show changes in color when enemy is attacking.

--- Enemy.cs
+++ Enemy.cs
@@ -5,20 +5,77 @@ using UnityEngine;
 [RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State { Idle, Chasing, Attacking };
+    State currentState;
+
     UnityEngine.AI.NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
 
-    public override void Start()
+    Color originalColour;
+
+    float attackDistanceThreshold = 0.5f;
+    float timeBetweenAttacks = 1;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
+
+    protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColour = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
         StartCoroutine(UpdatePath());
     }
 
     void Update()
     {
-        
+        if (timeBetweenAttacks.time > nextAttackTime)
+        {
+            float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold +myCollisionRadius + targetCollisionRadius, 2))
+            {
+                nextAttackTime = timeBetweenAttacks.time + tmeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 = dirToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius);
+        Vector3 originalPosition = transform.position;
+
+        float percent = 0;
+        float attackSpeed = 3;
+
+        skinMaterial.color = Color.purple;
+
+        while(percent <=1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent,2) + percent) *4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
@@ -27,8 +84,15 @@ public class Enemy : LivingEntity
 
         while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            pathfinder.SetDestination(target.position);
+            if (currentState = State.Chasing)
+            {
+                Vector3 = dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold/2);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(target.position);
+                }
+            }
             yield return new WaitForSeconds(refreshRate);
         }
     }
